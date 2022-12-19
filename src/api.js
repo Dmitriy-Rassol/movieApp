7df3755f9cb4e3a128519537a6acad981e678763
Rassolenk <rassoldima@yandex.ru>
2022-12-20 01:27:31 +0300

    Добавлен поиск и фильтрация

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const KEY = "?api_key=81c95d469f46ed667a73300bcb821603";
+export const KEY = "git";
 export const baseURL = `https://api.themoviedb.org/3`;
 export const langRU = `&language=ru-RU`;
 export const langEN = `&language=en-US`;
